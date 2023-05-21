beaf53693ff3a73aeda206f0344cc678272ed2c5
Han Byul <yahb9@naver.com>
2023-05-21 21:18:24 +0900

    Design: Home page 디자인

--- components/layouts/index.tsx
+++ components/layouts/index.tsx
@@ -1,10 +1,11 @@
-import { ReactElement } from 'react'
+import { ReactElement, useState } from 'react'
 import Image from 'next/image'
 
 import GNB from 'components/GNB'
 
 import AlarmIcon from 'public/svg/alarm.svg'
 import BasketIcon from 'public/svg/basket.svg'
+import SearchIcon from 'public/svg/search.svg'
 import styles from './layouts.module.scss'
 
 interface LayoutProps {
@@ -12,10 +13,24 @@ interface LayoutProps {
 }
 
 const Layout = ({ children }: LayoutProps) => {
+  const [isClickSearch, setIsClickSearch] = useState(false)
+
+  const handleSearchItemClick = () => {
+    setIsClickSearch((prevState) => !prevState)
+  }
+
   return (
     <div className={styles.background}>
       <div className={styles.layoutContainer}>
         <header className={styles.header}>
+          <Image
+            className={styles.icon}
+            src={SearchIcon}
+            alt='searchIcon'
+            width='20'
+            height='20'
+            onClick={handleSearchItemClick}
+          />
           <Image className={styles.icon} src={AlarmIcon} alt='alarmIcon' width='20' height='20' />
           <Image className={styles.icon} src={BasketIcon} alt='basketIcon' width='20' height='20' />
         </header>
